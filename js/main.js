dbef3878dac9f1bd853230a5ef9fd53b45a592ec
Andres Martinez <74833785+EditionDP@users.noreply.github.com>
2024-10-03 20:16:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropDowunMenu = document.querySelector('.dropdowun__menu');
 
 toggleBtnIcon.onclick = function(){
-    dropDowunMenu.classList.toggle('Close')
-    const isOpen = dropDowunMenu.classList.contains('Close')
+    dropDowunMenu.classList.toggle('close')
+    const isOpen = dropDowunMenu.classList.contains('close')
 
     toggleBtnIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
